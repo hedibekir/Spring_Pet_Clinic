6d392fb70be3237c75f23c7bb2fabe02b4c0feed
Hedi Bekir <hedi.bekir20@gmail.com>
2021-10-05 12:36:39 +0100

    POJOs

---
+++ src/main/java/guru/springtraining/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springtraining.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springtraining/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springtraining.petclinic.model;
+
+public class Vet extends Person{
+}
